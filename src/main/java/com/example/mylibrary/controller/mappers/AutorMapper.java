0690f37f932e040ee1db6742378ad30b849fb071
Gus <g.92oliveira@gmail.com>
2024-12-15 05:41:18 -0300

    mapeando

--- src/main/java/com/example/mylibrary/controller/mappers/AutorMapper.java
+++ src/main/java/com/example/mylibrary/controller/mappers/AutorMapper.java
@@ -1,12 +1,14 @@
 package com.example.mylibrary.controller.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.mylibrary.controller.dto.AutorDTO;
 import com.example.mylibrary.model.Autor;
 
 @Mapper(componentModel= "spring")
 public interface AutorMapper {
+    // @Mapping(source = "nome", target = "nome")
     Autor toEntity(AutorDTO dto);
     AutorDTO toDTO(Autor entity);
 }
