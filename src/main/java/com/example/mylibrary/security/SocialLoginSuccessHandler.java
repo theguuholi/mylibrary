2fac1f303918ce6319c891c07b601184d541c972
Gus <g.92oliveira@gmail.com>
2024-12-17 08:56:56 -0300

    social login

--- src/main/java/com/example/mylibrary/security/SocialLoginSuccessHandler.java
+++ src/main/java/com/example/mylibrary/security/SocialLoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.example.mylibrary.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,6 +9,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.example.mylibrary.model.User;
 import com.example.mylibrary.service.UserService;
 
 import jakarta.servlet.ServletException;
@@ -19,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SocialLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    private static final String DEFAULT_PASSWORD = "123123123";
     private final UserService service;
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -28,6 +31,17 @@ public class SocialLoginSuccessHandler extends SavedRequestAwareAuthenticationSu
         var principal = oauth.getPrincipal();
         String email = principal.getAttribute("email");
         var user = service.findByEmail(email);
+
+        if (user == null) {
+            user = new User();
+            user.setEmail(email);
+            var login = email.substring(0, email.indexOf('@'));
+            user.setLogin(login);
+            user.setPassword(DEFAULT_PASSWORD);
+            user.setRoles(List.of("USER"));
+            service.save(user);
+        }
+
         var customAuth = new CustomAuthentication(user);
         SecurityContextHolder.getContext().setAuthentication(customAuth);
         super.onAuthenticationSuccess(request, response, customAuth);
